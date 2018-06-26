a4586b4b31f725d954670c65911e76ad8f205e3a
노경모 <kyeongmo2@gmail.com>
2018-06-26 23:20:15 +0900

    Create development branch and install webpack-dev-server and add dev server options to config file

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   mode: 'development',
   // mode: 'production',
 
+  devtool: 'inline-source-map',
+
   entry: {
     app: path.resolve(__dirname, 'src', 'app.js'),
     bar: path.resolve(__dirname, 'src', 'bar.js')
@@ -23,5 +25,38 @@ module.exports = {
       inject: 'body',
       template: path.resolve(__dirname, 'index.html')
     })
-  ]
+  ],
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 8000,
+    proxy: {
+      '/api/*': 'http://localhost:3000'
+    },
+    // Enable gzip compression
+    compress: true,
+    // True for index.html upon 404s, object for multiple paths
+    historyApiFallback: true,
+    // Inline mode is recommended for Hot Module Replacement as it includes an HMR trigger from the websocket
+    inline: true,
+    // Hot module replacement. Depends on HotModuleReplacementPlugin
+    hot: true,
+    // Shows a full-screen overlay in the browser when there are compiler errors or warnings
+    overlay: true,
+    // filename: 'static/js/bundle.js',
+    // CLI custom
+    stats: {
+      assets: true,           // Sort assets by a field
+      children: true,         // Add chunk information
+      chunks: false,          // Add built modules information to chunk information
+      chunkModules: false,
+      colors: true,
+      performance: false,     // Show performance hint when file size exceeds `performance.maxAssetSize`
+      publicPath: true,       // Add public path information
+      version: true,
+      hash: true,             // Add the hash of the compilation
+      timings: false,
+      warnings: false
+    }
+  }
 };
